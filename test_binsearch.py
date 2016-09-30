bd1d9a76a05995e32efa212a01297076b75c255b
JunZhou1544 <judyzhou0201@gmail.com>
2016-09-30 00:50:08 -0400

    change test case

--- test_binsearch.py
+++ test_binsearch.py
@@ -1,19 +1,18 @@
-
 from pytest import raises
 from binsearch import binary_search
 import numpy as np
 
 def test_wired_data_1():
     with raises(TypeError):
-        binary_search([None, None],1) 
+        binary_search([None, None],1)
 
 def test_wired_data_2():
     with raises(TypeError):
-        binary_search(["a", "b"],1) 
+        binary_search(["a", "b"],1)
 
 def test_size_1_1():
     assert binary_search([5], 5) == 0
-    
+
 def test_size_1_2():
     assert binary_search([4], 5) == -1
 
@@ -22,7 +21,7 @@ def test_size_0():
 
 def test_inf_1():
     assert binary_search([1,2,np.inf], 2) == 1
-    
+
 def test_inf_1():
     assert binary_search([1,2,np.inf], np.inf) == 2
 
@@ -34,7 +33,7 @@ def test_search_out_of_bound_2():
 
 def test_search_out_of_bound_3():
     with raises(TypeError):
-        assert binary_search([1,2,3,4,5], "a") 
+        assert binary_search([1,2,3,4,5], "a")
 
 def test_search_out_of_bound_4():
     assert binary_search([1,2,3,4,5],1,3,4) == -1
@@ -49,4 +48,4 @@ def test_search_in_bound_2():
     assert binary_search([1,2,3,4,5],3,0,3) == 2
 
 def test_multiple_num():
-    assert binary_search([1,2,2,3,4,5],2) == (1 or 2)
+    assert binary_search([1,2,2,3,4,5],2) == 1 or binary_search([1,2,2,3,4,5],2) == 2
